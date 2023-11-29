24a44836181c1a7b41b6d8914f8cef7783b715bd
Alexey Kleinerman <agmcworkmail@gmail.com>
2023-11-29 20:42:42 +0100

    Life could be dream

--- First Year/Principles of computer programming/FinalProject/FinalProject/Battleship.c
+++ First Year/Principles of computer programming/FinalProject/FinalProject/Battleship.c
@@ -3,12 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-int answer = 0;
-
 typedef struct {
 	char board[8][8];
-	int ship[5][5];
-	int shoots, hits, kills;
+	int ship[5];
+	int shoots, hits;
 } side;
 
 
@@ -19,23 +17,56 @@ void removeElement(int *array, int index, int *size)
 }
 
 
-int checkPossibility(int* cells, int* numbers, int cellsSize, int numberSize)
+void generation(side* board)
 {
-	int counter = 0;
+	int numberSize = 64, cell;
+	int *numbers = calloc(numberSize, sizeof(int));
+	srand((unsigned int)time(NULL));
 
-	for (int i = 0; i < cellsSize; i++)
+	for (int i = 0; i < numberSize; i++)
 	{
-		for (int j = 0; j < numberSize; j++)
+		numbers[i] = i;
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		int check = 1;
+
+		while (check)
 		{
-			if (cells[i] == numbers[j])
+			cell = numbers[rand() % numberSize];
+			board->ship[i] = cell;
+
+			for (int j = 0; j < numberSize; j++)
 			{
-				counter++;
-				break;
+				if (cell == numbers[j])
+				{
+					check = 0;
+					break;
+				}
+			}
+
+		}
+
+		int cells[9] = { cell - 9, cell - 8, cell - 7, cell - 1, cell, cell + 1, cell + 7, cell + 8, cell + 9 };
+
+		for (int j = 0; j < 9; j++)
+		{
+			for (int g = 0; g < numberSize; g++)
+			{
+				if (cells[j] == numbers[g])
+				{
+					removeElement(numbers, g, &numberSize);
+					break;
+				}
 			}
 		}
 	}
+}
 
-	if (counter == cellsSize)
+int checkWin(side *player)
+{
+	if (player->hits == 5)
 	{
 		return 0;
 	}
@@ -46,114 +77,7 @@ int checkPossibility(int* cells, int* numbers, int cellsSize, int numberSize)
 }
 
 
-void generation(side* board)
-{
-	int numberSize = 64, firstCell;
-	int *numbers = calloc(numberSize, sizeof(int));
-	srand((unsigned int)time(NULL));
-
-	for (int i = 5; i > 0; i--)
-	{
-		int check = 1;
-
-		while (check)
-		{
-			firstCell = numbers[rand() % numberSize];
-			int *nextCells = calloc(i - 1, sizeof(int));
-
-			board->ship[i - 1][0] = firstCell;
-
-			if (((firstCell + i - 1) / 8) == (firstCell / 8)) // right 
-			{
-				for (int j = 0; j < i - 1; j++)
-				{
-					nextCells[j] = firstCell + j + 1;
-					board->ship[i - 1][j + 1] = nextCells[j];
-
-					printf("%d", nextCells[j]);
-					
-				}
-
-				check = checkPossibility(nextCells, numbers, i - 1, numberSize);
-				
-				break;
-			}
-			else if (((firstCell - i + 1) / 8) == (firstCell / 8)) // left
-			{
-				for (int j = 0; j < i - 1; j++)
-				{
-					nextCells[j] = firstCell - j - 1;
-					board->ship[i - 1][j + 1] = nextCells[j];
-				}
-
-				check = checkPossibility(nextCells, numbers, i - 1, numberSize);
-			}
-			else if (firstCell + 8 * (i - 1) < 64) // down
-			{
-				for (int j = 0; j < i - 1; j++)
-				{
-					nextCells[j] = firstCell + ((j + 1) * 8);
-					board->ship[i - 1][j + 1] = nextCells[j];
-				}
-
-				check = checkPossibility(nextCells, numbers, i - 1, numberSize);
-			}
-			else if (firstCell - 8 * (i - 1) > -1) // up
-			{
-				for (int j = 0; j < i - 1; j++)
-				{
-					nextCells[j] = firstCell - ((j + 1) * 8);
-					board->ship[i - 1][j + 1] = nextCells[j];
-				}
-
-				check = checkPossibility(nextCells, numbers, i - 1, numberSize);
-			}
-
-		}
-
-		printf("gen suc");
-
-		for (int j = 0; j < 5; j++)
-		{
-			if (board->ship[i - 1][j] != -1)
-			{
-				for (int g = 0; g < numberSize; g++)
-				{
-					int cell = board->ship[i - 1][j];
-					int cellVariations[9] = { cell - 9, cell - 8, cell - 7, cell - 1, cell, cell + 1, cell + 7, cell + 8, cell + 9 };
-
-					for (int k = 0; k < 9; k++)
-					{
-						if (cellVariations[k] == numbers[g])
-						{
-							removeElement(numbers, g, &numberSize);
-						}
-					}
-				}
-			}
-		}
-
-	}
-}
-
-
-checkAnswer(int canswer)
-{
-	if (canswer == 0)
-	{
-		answer = 0;
-		scanf("%*[^\n]%*c");
-	}
-}
-
-
-int checkWin()
-{
-
-}
-
-
-displayBoard(char* board)
+void displayBoard(side *player)
 {
 	char abc[8] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' };
 
@@ -161,152 +85,219 @@ displayBoard(char* board)
 
 	for (int i = 0; i < 8; i++)
 	{
-		printf("  ------------------\n");
+		printf("  -----------------\n");
 		printf("%c |", abc[i]);
 
 		for (int j = 0; j < 8; j++)
 		{
-			printf("%с|", *(board + i * 8 + j));
+			printf("%c|", player->board[i][j]);
 		}
 
 		printf("\n");
 	}
+	printf("  -----------------\n");
 }
 
-
-int playerTurn(side* player1, side* player2)
+void displayInfo(side* player, side* computer)
 {
+	printf("\nYour board:\n");
+	displayBoard(player);
+	printf("\nYour shoots: %d\nYour hits: %d\nYour ships: %d\n", player->shoots, player->hits, (5 - computer->hits));
 
-	printf("Your board:\n");
-	displayBoard(player1->board);
-
-	printf("Opponent's board:\n");
-	displayBoard(player2->board);
-
-	return checkWin();
+	printf("\nComputer's board:\n");
+	displayBoard(computer);
+	printf("\nComputer's shoots: %d\nComputer's hits: %d\nComputer's ships: %d\n", computer->shoots, computer->hits, (5 - player->hits));
 }
 
 
-int computerTurn(side* computer, side* player)
+int playerTurn(side* player, side* computer)
 {
-	printf("Your board:\n");
-	displayBoard(player->board);
+	printf("\nEnter the cell you want to shoot at (e.g. 'A2' CUPS only):\n");
+	printf("If you want to exit to menu type '11' if you want to save type '22'\n");
+	char ans[] = "00";
 
-	printf("Opponent's board:\n");
-	displayBoard(computer->board);
-
-	return checkWin();
-}
-
-
-int game(int turn, int mode, side* player1, side* player2) {
-
-	if (mode == 1)
+	while(1)
 	{
-		if (turn % 2 == 0)
+		scanf("%2s", ans);
+		scanf("%*[^\n]%*c");
+
+		if (ans[0] == '1' && ans[1] == '1')
 		{
-			return playerTurn(player1, player2);
+			return 0;
+		}
+		else if(ans[0] == '2' && ans[1] == '2')
+		{
+			printf("\nGame successfuly saved\n");
+		}
+		else if (((int)ans[0]) >= 65 && ((int)ans[0] <= 72) && ((int)ans[1] >= 49) && ((int)ans[1] <= 56))
+		{
+			if (computer->board[(int)ans[0] - 65][(int)ans[1] - 49] == ' ')
+			{
+				int onFire = 0;
+
+				for (int i = 0; i < 5; i++)
+				{
+					if (computer->ship[i] == (((int)ans[0] - 65) * 8 + ((int)ans[1] - 49)))
+					{
+						onFire = 1;
+						break;
+					}
+				}
+
+				if (onFire)
+				{
+					printf("\nYou sank the computer ship!\n");
+					computer->board[(int)ans[0] - 65][(int)ans[1] - 49] = 'X';
+					player->hits += 1;
+				}
+				else
+				{
+					printf("\nYou missed\n");
+					computer->board[(int)ans[0] - 65][(int)ans[1] - 49] = 'O';
+				}
+
+				player->shoots += 1;
+				break;
+			}
+			else
+			{
+				printf("\nYou have already shooted in this cell\n");
+			}
 		}
 		else
 		{
-			return computerTurn(player2, player1);
+			printf("\nWrong answer, please, type correcly\n");
 		}
+
+	}
+
+	displayInfo(player, computer);
+
+	if (checkWin(player))
+	{
+		return 1;
 	}
 	else
 	{
-		if (turn % 2 == 0)
-		{
-			return playerTurn(player1, player2);
-		}
-		else
-		{
-			return playerTurn(player2, player1);
-		}
+		printf("\nPlayer wins!\n");
+		return 0;
+	}
+}
+
+
+int computerTurn(side *computer, side *player,int *numbers, int *numberSize)
+{
+	srand((unsigned int)time(NULL));
+	int cell = numbers[rand() % *numberSize];
+
+	printf("\nComputer shooted to: %c%d\n", (65 + (cell / 8)), ((cell % 8) + 1));
+
+	if (player->board[cell / 8][cell % 8] == 'S')
+	{
+		printf("\nComputer hitted your ship!\n");
+		player->board[cell / 8][cell % 8] = 'X';
+		computer->hits += 1;
+	}
+	else
+	{
+		printf("\nComputer missed!\n");
+		player->board[cell / 8][cell % 8] = 'O';
+	}
+
+	computer->shoots += 1;
+	removeElement(numbers, cell, numberSize);
+
+	displayInfo(player, computer);
+
+	if (checkWin(computer))
+	{
+		return 1;
+	}
+	else
+	{
+		printf("\nComputer wins\n");
+		return 0;
+	}
+}
+
+
+int game(int turn, side *player, side *computer, int* numbers, int* numberSize) {
+	if (turn % 2 == 0)
+	{
+		return playerTurn(player, computer);
+	}
+	else
+	{
+		return computerTurn(computer, player, numbers, numberSize);
 	}
 }
 
 
 int main()
 {
+	int canswer = 0, answer = 0, turn = 0;
+	side player, bot;
+
+	memset(player.board, ' ', sizeof(player.board));
+	memset(player.ship, -1, sizeof(player.ship));
+	player.shoots = 0;
+	player.hits = 0;
+
+	memset(bot.board, ' ', sizeof(bot.board));
+	memset(bot.ship, -1, sizeof(bot.ship));
+	bot.shoots = 0;
+	bot.hits = 0;
+
+	static int numberSize = 64;
+	int* numbers = calloc(numberSize, sizeof(int));
+
+	for (int i = 0; i < numberSize; i++)
+	{
+		numbers[i] = i;
+	}
+
 	printf("Welcome to Battleship game!\n");
 
 	while (answer != 3)
 	{
 		printf("\nPlease, choose option (1/2/3):\n1. Play game\n2. Load Game\n3. Exit\n");
-		checkAnswer(scanf("%d", &answer));
+		canswer = scanf("%d", &answer);
+		
+		if (canswer == 0)
+		{
+			answer = 0;
+		}
 
+		scanf("%*[^\n]%*c");
 
 		switch (answer)
 		{
 		case 1:
-			printf("\nChoose option (1/2/3):\n1. Singleplayer (Player vs Bot)\n2. Multiplayer (Player vs Player)\n3. Exit\n");
+			generation(&player);
+			generation(&bot);
 
-			int mode, turn = 0;
-
-			while (1)
+			for (int i = 0; i < 5; i++)
 			{
-				checkAnswer(scanf("%d", &answer));
-
-				if (answer == 1)
+				for (int j = 0; j < 8; j++)
 				{
-					printf("\nsingi\n");
-
-					mode = 1;
-					side player, bot;
-
-					memset(player.board, ' ', sizeof(player.board));
-					memset(player.ship, -1, sizeof(player.ship));
-					player.shoots = 0;
-					player.hits = 0;
-					player.kills = 0;
-
-					memset(bot.board, ' ', sizeof(bot.board));
-					memset(bot.ship, -1, sizeof(bot.ship));
-					bot.shoots = 0;
-					bot.hits = 0;
-					bot.kills = 0;
-
-					generation(&player);
-					generation(&bot);
-
-
-					while (game(turn, mode, &player, &bot))
+					for (int g = 0; g < 8; g++)
 					{
-						turn++;
+						if (player.ship[i] == ((j * 8) + g))
+						{
+							player.board[j][g] = 'S';
+						}
 					}
-
-					break;
-				}
-				else if (answer == 2)
-				{
-					printf("\nmulti\n");
-
-					mode = 2;
-					side player1, player2;
-
-					memset(player1.board, ' ', sizeof(player1.board));
-					memset(player2.board, ' ', sizeof(player2.board));
-
-					while (game(turn, mode, &player1, &player2))
-					{
-						turn++;
-					}
-
-					break;
-				}
-				else if (answer == 3)
-				{
-					break;
-				}
-				else
-				{
-					printf("\nWrong answer, please, type '1' or '2' or '3'.\n");
 				}
 			}
 
+
+			while (game(turn, &player, &bot, numbers, &numberSize))
+			{
+				turn++;
+			}
 			break;
 		case 2:
-			printf("B");
+			printf("Deadline is very soon");
 			break;
 		case 3:
 			break;
@@ -318,13 +309,3 @@ int main()
 
 	return 0;
 }
-
-
-//   1 2 3 4
-//  ---------
-// A| |O| | |
-//  ---------
-// B| | |X| |
-//  ---------
-// C|O| | |X|
-//  ---------
